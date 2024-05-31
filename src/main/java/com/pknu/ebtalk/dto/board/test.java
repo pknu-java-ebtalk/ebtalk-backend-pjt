158d0e58afe738a46980fc248c20117ef8c0170e
GyeongHo <kungho84@gmail.com>
2024-05-31 09:14:20 +0900

    .

--- src/main/java/com/pknu/ebtalk/dto/board/test.java
+++ src/main/java/com/pknu/ebtalk/dto/board/test.java
@@ -1,4 +1,13 @@
 package com.pknu.ebtalk.dto.board;
 
 public class test {
+    private int b_no;
+    private String b_user_id;
+    private int b_category_no;
+    private String b_title;
+    private String b_content;
+    private int b_views;
+    private String b_modified_at;
+
+    private String b_created_at;
 }
