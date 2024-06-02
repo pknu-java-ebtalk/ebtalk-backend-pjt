3fffcf10f56656cc6b594698d460512faed3139e
iieunji023 <iieunji023@gmail.com>
2024-06-02 20:55:59 +0900

    Feat:추가 스터디 모집 기능
    스터디 모집글 등록, 수정, 삭제, 리스트/상세페이지 조회 기능 완료

--- src/main/java/com/pknu/ebtalk/service/study/IStudyService.java
+++ src/main/java/com/pknu/ebtalk/service/study/IStudyService.java
@@ -1,10 +1,21 @@
 package com.pknu.ebtalk.service.study;
 
 import com.pknu.ebtalk.dto.study.StudyDto;
-import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 public interface IStudyService {
     // 스터디 모집 등록
-    void insertStudyConfirm(StudyDto studyDto);
+    StudyDto insertStudyConfirm(StudyDto studyDto);
+
+    // 스터디 모집 수정
+    StudyDto selectStudyInfoByNo(int no);
+
+    StudyDto updateStudyConfirm(StudyDto studyDto);
+
+    // 스터디 모집 리스트
+    List<StudyDto> selectStudyAllList();
+
+    void deleteStudyConfirm(int no);
 }
