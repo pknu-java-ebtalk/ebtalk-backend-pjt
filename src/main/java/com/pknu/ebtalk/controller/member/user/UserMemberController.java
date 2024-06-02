8330f9bf90496fac93faa7a2f8cb9dd633e30aea
hyanyul <tjdwl0808@gmail.com>
2024-06-02 18:36:43 +0900

    Feat: 추가 회원가입 기능 유효성 검사 회원가입 시 비어있는 칸 UI 바뀌게, 비밀번호와 비밀번호 확인의 값이 같지 않으면 DB에 저장되지 않고 다시 회원가입으로 돌아오도록 기능 추가

--- src/main/java/com/pknu/ebtalk/controller/member/user/UserMemberController.java
+++ src/main/java/com/pknu/ebtalk/controller/member/user/UserMemberController.java
@@ -3,16 +3,18 @@ package com.pknu.ebtalk.controller.member.user;
 import com.pknu.ebtalk.dto.member.UserMemberDto;
 import com.pknu.ebtalk.dto.study.StudyDto;
 import com.pknu.ebtalk.service.member.user.UserMemberService;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Member;
 
 @Log4j2
 @Controller
@@ -24,18 +26,22 @@ public class UserMemberController {
 
     // 회원가입
     @GetMapping(value={"/sign_up"})
-    public String userSignUp() {
+    public String userSignUp(Model model) {
         log.info("[UserMemberController] userSignUp()");
 
-        String nextPage = "/html/member/user_sign_up";
+        model.addAttribute("userMemberDto", new UserMemberDto());
 
-        return nextPage;
+        return "/html/member/user_sign_up";
     }
 
     // 회원가입 정보 받음
     @PostMapping("/sign_up_confirm")
-    public String userSignUpConfirm(UserMemberDto userMemberDto, Model model) {
+    public String userSignUpConfirm(@Valid @ModelAttribute UserMemberDto userMemberDto, BindingResult result, Model model) {
         log.info("[UserMemberController] userSignUpConfirm()");
+
+        if (result.hasErrors() || !userMemberDto.getPw().equals(userMemberDto.getPw_check())){
+            return "/html/member/user_sign_up";
+        }
 
         userMemberService.insertUserSignUpConfirm(userMemberDto);
 
--- src/main/java/com/pknu/ebtalk/dto/member/UserMemberDto.java
+++ src/main/java/com/pknu/ebtalk/dto/member/UserMemberDto.java
@@ -1,32 +1,38 @@
 package com.pknu.ebtalk.dto.member;
 
+import com.pknu.ebtalk.service.member.user.PwConfirmCheck;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
+@PwConfirmCheck(pw = "pw", pw_check = "pw_check")
 public class UserMemberDto {
-//    @NotBlank
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String id;          // 사용자 아이디(사용자)
 
-//    @NotBlank
+    @NotNull
+    @Range(min=1, max=3, message = "교육과정을 선택해주세요.")
     private int edu_no;         // 교육과정 번호(사용자)
     private int no;             // 교육과정 번호(교육훈련과정)
     private String type;        // 과목명(교육훈련과정)
     private String started_at;  // 시작일(교육훈련과정)
     private String finished_at; // 종료일(교육훈련과정)
 
-//    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String pw;          // 비밀번호(사용자)
     private String pw_check;    // 비밀번호 확인
 
-//    @NotBlank
+    @NotBlank(message = "전화번호를 입력해주세요.")
     private String phone;       // 전화번호(사용자)
 
-//    @NotBlank
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;        // 이름(사용자)
 
     private String profile_img; // 프로필 사진(사용자)
